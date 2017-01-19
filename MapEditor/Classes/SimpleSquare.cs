6ea8976f681a19a2f6766967fd418651695c178c
Bernard Walker <guidedbycthulhu@gmail.com>
2017-01-18 22:23:38 -0500

    Quick start to a basic map editor

---
+++ MapEditor/Classes/SimpleSquare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor.Classes
+{
+    public class SimpleSquare
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+    }
+}
---
+++ MapEditor/ViewModels/MapViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MapEditor.ViewModels
+{
+    public class MapViewModel : INotifyPropertyChanged
+    {
+        #region  OnPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Called when [property changed].
+        /// </summary>
+        /// <param name="name">The name.</param>
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) { handler(this, new PropertyChangedEventArgs(name)); }
+        }
+        #endregion
+
+        public MapViewModel()
+        {
+            GenerateMap();
+        }
+
+        private void GenerateMap()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for(int j = 0; j < Size; j++)
+                {
+                    Map.Add(new Classes.SimpleSquare() { Row = i, Column = j });
+                }
+            }
+        }
+
+        private int mSize = 15;
+        public int Size
+        {
+            get { return mSize; }
+            set
+            {
+                mSize = value;
+                GenerateMap();
+                OnPropertyChanged("Size");
+                OnPropertyChanged("Map");
+            }
+        }
+
+        private ObservableCollection<Classes.SimpleSquare> mMap = new ObservableCollection<Classes.SimpleSquare>();
+        public ObservableCollection<Classes.SimpleSquare> Map
+        {
+            get { return mMap; }
+        }
+    }
+}
